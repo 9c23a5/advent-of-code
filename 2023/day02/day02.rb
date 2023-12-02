03e3ef90a8d436331453a26b67b20837de933515
Shay Mejuto <40715125+9c23a5@users.noreply.github.com>
2023-12-02 16:05:05 +0100

    Complete day 02 part 1

--- 2023/day02/day02.rb
+++ 2023/day02/day02.rb
@@ -2,4 +2,47 @@
 
 require_relative "../../lib/aoc_helpers"
 
-input = load_input
+class Game
+  attr_reader :id, :max_red, :max_green, :max_blue
+  def initialize(id, params)
+    @id = id
+    @max_red = params[:red]
+    @max_green = params[:green]
+    @max_blue = params[:blue]
+  end
+
+  def self.parse(string)
+    params = {}
+
+    %i[red green blue].each do |color|
+      color_regex = /(\d+)\s#{color}/
+      amount = string.scan(color_regex).flatten.map(&:to_i).max
+
+      params[color] = amount
+    end
+
+    game_id = string.match(/Game (\d+):/).captures.first.to_i
+
+    Game.new(game_id, params)
+  end
+
+  def possible?(params)
+    @max_red <= params[:red] &&
+      @max_blue <= params[:blue] &&
+      @max_green <= params[:green]
+  end
+end
+
+input = load_input.split("\n")
+
+# Part 1
+
+all_games = input.map { |game_line|
+  Game.parse(game_line)
+}
+
+result = all_games.select { |game|
+  game.possible?({red: 12, green: 13, blue: 14})
+}.map(&:id).sum
+
+puts "Result for part 1: #{result}"
--- specs/2023/day02_spec.rb
+++ specs/2023/day02_spec.rb
@@ -1,3 +1,87 @@
 # frozen_string_literal: true
 
 require_relative "../../2023/day02/day02"
+
+RSpec.describe Game do
+  let(:maximum_possible_blocks) {
+    {
+      red: 12,
+      green: 13,
+      blue: 14
+    }
+  }
+
+  context "unit test" do
+    subject { described_class.parse(game_line) }
+
+    context "single possible game" do
+      let(:game_line) {
+        "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+      }
+
+      it "gets the correct maximum blocks possible" do
+        expect(subject.max_red).to eq 4
+        expect(subject.max_green).to eq 2
+        expect(subject.max_blue).to eq 6
+      end
+
+      it "parses game id" do
+        expect(subject.id).to eq 1
+      end
+
+      it "returns true for possible game" do
+        expect(
+          subject.possible?(maximum_possible_blocks)
+        ).to be true
+      end
+    end
+
+    context "single impossible game" do
+      let(:game_line) {
+        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
+      }
+
+      it "gets the correct maximum blocks possible" do
+        expect(subject.max_red).to eq 20
+        expect(subject.max_green).to eq 13
+        expect(subject.max_blue).to eq 6
+      end
+
+      it "parses game id" do
+        expect(subject.id).to eq 3
+      end
+
+      it "returns false for possible game" do
+        expect(
+          subject.possible?(maximum_possible_blocks)
+        ).to be false
+      end
+    end
+  end
+
+  context "integration test" do
+    subject {
+      puzzle_input.split("\n").map { |game_line|
+        described_class.parse(game_line)
+      }
+    }
+
+    let(:puzzle_input) {
+      <<~INPUT
+        Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+      INPUT
+    }
+
+    it "sums ids of only possible games" do
+      expect(
+        subject.select { |game|
+          game.possible?(maximum_possible_blocks)
+        }.map(&:id).sum
+      ).to eq 8
+    end
+  end
+end
