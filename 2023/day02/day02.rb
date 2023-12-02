eac52056d0299fa0f48b5955b8deeb399c99a667
Shay Mejuto <40715125+9c23a5@users.noreply.github.com>
2023-12-02 16:33:17 +0100

    Respec in preparation for part 2

--- 2023/day02/day02.rb
+++ 2023/day02/day02.rb
@@ -3,12 +3,13 @@
 require_relative "../../lib/aoc_helpers"
 
 class Game
-  attr_reader :id, :max_red, :max_green, :max_blue
+  attr_reader :id, :red_played, :green_played, :blue_played
   def initialize(id, params)
     @id = id
-    @max_red = params[:red]
-    @max_green = params[:green]
-    @max_blue = params[:blue]
+    # Array of numbers played for each color...
+    @red_played = params[:red]
+    @green_played = params[:green]
+    @blue_played = params[:blue]
   end
 
   def self.parse(string)
@@ -16,9 +17,9 @@ class Game
 
     %i[red green blue].each do |color|
       color_regex = /(\d+)\s#{color}/
-      amount = string.scan(color_regex).flatten.map(&:to_i).max
+      played_amounts = string.scan(color_regex).flatten.map(&:to_i)
 
-      params[color] = amount
+      params[color] = played_amounts
     end
 
     game_id = string.match(/Game (\d+):/).captures.first.to_i
@@ -27,22 +28,24 @@ class Game
   end
 
   def possible?(params)
-    @max_red <= params[:red] &&
-      @max_blue <= params[:blue] &&
-      @max_green <= params[:green]
+    @red_played.max <= params[:red] &&
+      @blue_played.max <= params[:blue] &&
+      @green_played.max <= params[:green]
   end
 end
 
 input = load_input.split("\n")
 
-# Part 1
-
 all_games = input.map { |game_line|
   Game.parse(game_line)
 }
 
+# Part 1
+
+my_maximum = {red: 12, green: 13, blue: 14}
+
 result = all_games.select { |game|
-  game.possible?({red: 12, green: 13, blue: 14})
+  game.possible?(my_maximum)
 }.map(&:id).sum
 
 puts "Result for part 1: #{result}"
